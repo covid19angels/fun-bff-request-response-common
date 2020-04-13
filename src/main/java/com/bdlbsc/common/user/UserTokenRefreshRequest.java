633dd21edf9a579087a994fb5fabe1b33d3012f5
m_maohuawei <maohuawei@bdlbsc.com>
2020-04-13 17:49:54 +0800

    增加token相关api

--- src/main/java/com/bdlbsc/common/user/UserTokenRefreshRequest.java
+++ src/main/java/com/bdlbsc/common/user/UserTokenRefreshRequest.java
@@ -1,19 +1,16 @@
 package com.bdlbsc.common.user;
 
-
-import com.bdlbsc.common.base.BaseRequest;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
-
-// 刷新token
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class UserTokenRefreshRequest  extends BaseRequest {
-    private String access_token;
+public class UserTokenRefreshRequest {
+
+    private String refresh_token;
+
 }
--- src/main/java/com/bdlbsc/common/user/UserTokenRefreshResponse.java
+++ src/main/java/com/bdlbsc/common/user/UserTokenRefreshResponse.java
@@ -1,20 +1,16 @@
 package com.bdlbsc.common.user;
 
-
-
-
-import com.bdlbsc.common.base.BaseResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
-// 刷新token
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class UserTokenRefreshResponse extends BaseResponse {
+public class UserTokenRefreshResponse {
+
     private String access_token;
+
 }
