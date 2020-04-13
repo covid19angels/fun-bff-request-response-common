32dc272e7f43a93bc20f81bc4e2e4a0c076afb04
m_maohuawei <maohuawei@bdlbsc.com>
2020-04-13 17:47:32 +0800

    增加token相关api

--- src/main/java/com/bdlbsc/common/api/USERAPI.java
+++ src/main/java/com/bdlbsc/common/api/USERAPI.java
@@ -2,11 +2,20 @@ package com.bdlbsc.common.api;
 
 public interface USERAPI {
 
+    // 用户名登录
     String LOGIN_USERNAME = "user/login/username";
+    // 手机号登录请求验证码
     String LOGIN_PHONENUMBER_REQUEST_VERIFICATIONCODE = "user/login/phonenumber-request-verificationcode";
+    // 手机号登录
     String LOGIN_PHONENUMBER = "user/login/phonenumber";
+    // 手机号注册请求验证码
     String REGISTERED_PHONENUMBER_REQUEST_VERIFICATIONCODE = "user/registered/phonenumber-request-verificationcode";
+    // 手机号注册
     String REGISTERED_PHONENUMBER = "user/registered/phonenumber";
+    // 用户名注册
     String REGISTERED_USERNAME = "user/registered/username";
-
+    // 检查token是否过期
+    String TOKEN_CHECK = "user/token/check";
+    // 刷新token
+    String TOKEN_REFRESH = "user/token/refresh";
 }
---
+++ src/main/java/com/bdlbsc/common/user/UserTokenCheck.java
@@ -0,0 +1,16 @@
+package com.bdlbsc.common.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserTokenCheck {
+
+    private String access_token;
+
+}
