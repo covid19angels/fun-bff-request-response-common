8daa3738b16a7f4b3433d7d261b55eff5b73eb74
m_maohuawei <maohuawei@bdlbsc.com>
2020-04-13 10:25:33 +0800

    add api

---
+++ src/main/java/com/bdlbsc/common/api/USERAPI.java
@@ -0,0 +1,12 @@
+package com.bdlbsc.common.api;
+
+public interface USERAPI {
+
+    String LOGIN_USERNAME = "login/username";
+    String LOGIN_PHONENUMBER_REQUEST_VERIFICATIONCODE = "login/phonenumber-request-verificationcode";
+    String LOGIN_PHONENUMBER = "login/phonenumber";
+    String REGISTERED_PHONENUMBER_REQUEST_VERIFICATIONCODE = "registered/phonenumber-request-verificationcode";
+    String REGISTERED_PHONENUMBER = "registered/phonenumber";
+    String REGISTERED_USERNAME = "registered/username";
+
+}
